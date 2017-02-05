949411975575df5a89de5614e3a6f4254e2d3061
Xavier <xcoadic@protonmail.com>
2017-02-05 17:35:51 +0100

    Update test_blink_program.ino

--- ByodIT/test_code/test_blink_program.ino
+++ ByodIT/test_code/test_blink_program.ino
@@ -1,5 +1,5 @@
-\\ The Adafruit Feather HUZZAH is an Arduino-compatible microcontroller with a built-in WiFi chip. 
-\\ It is therefore pretty useful for WiFi-enabled electronics projects. Plug your Feather into your computer, and run the following program. The onboard LED should blink. Try changing the timing of the blinking, too, to ensure that your program is being run correctly.
+// The Adafruit Feather HUZZAH is an Arduino-compatible microcontroller with a built-in WiFi chip. 
+// It is therefore pretty useful for WiFi-enabled electronics projects. Plug your Feather into your computer, and run the following program. The onboard LED should blink. Try changing the timing of the blinking, too, to ensure that your program is being run correctly.
 
 void setup() {
   pinMode(0, OUTPUT);
